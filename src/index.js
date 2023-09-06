5f5800a48436c40b3f302ae24d3cbcf1878ec4ed
josuehoenicka <josuee.vzla@gmail.com>
2023-09-06 20:45:14 -0300

    feat: main [app.get]

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
 
+/* ROUTES */
+app.get('/', (req, res) => {
+    res.send("Josue Hoenicka");
+})
+
 app.listen(port, () => console.log("Server working on port ", port));
