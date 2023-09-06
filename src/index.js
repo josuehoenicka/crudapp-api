a43cf64843e75defc41b69c1650a229c60e84bc2
josuehoenicka <josuee.vzla@gmail.com>
2023-09-06 20:54:40 -0300

    feat: main [dotenv installation]

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
+const mongoose = requite("mongoose");
 
 const app = express();
 const port = process.env.PORT || 9000;
 
 /* ROUTES */
-app.get('/', (req, res) => {
-    res.send("Josue Hoenicka");
-})
+app.get("/", (req, res) => {
+  res.send("Josue Hoenicka");
+});
+
+/* MONGODB CONNECTION */
 
 app.listen(port, () => console.log("Server working on port ", port));
